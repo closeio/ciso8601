ec049b4340b5187edb22060151b40219b6de4b79
Low Kian Seong <kianseong@gmail.com>
2014-06-10 06:25:38 +0800

    Update setup.py
    Adding a line of description to project so that project is not listed as 'UNKNOWN' for description both on pypi and pip searches.

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, Extension
 setup(
     name="ciso8601",
     version="1.0.1",
+    description=('Library to converts ISO8601 date time '
+                 'strings into Python datetime objects'),
     ext_modules=[Extension("ciso8601", ["module.c"])],
     test_suite='tests',
     tests_require=['pytz'],
