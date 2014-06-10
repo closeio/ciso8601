166f69b6442ee83c1c835fb89fe1ba3b3c19f2dd
Thomas Steinacher <tom@eggdrop.ch>
2014-06-09 18:28:07 -0700

    Adding project description to setup.py, thanks lowks!

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, Extension
 setup(
     name="ciso8601",
     version="1.0.1",
+    description='Fast ISO8601 date time parser for Python written in C',
     ext_modules=[Extension("ciso8601", ["module.c"])],
     test_suite='tests',
     tests_require=['pytz'],
