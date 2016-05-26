c7ff83865e0fbb69e7386f3ecbf15d84c06bc407
Thomas Steinacher <tom@eggdrop.ch>
2016-05-25 17:20:05 -0700

    Adding classifiers

--- setup.py
+++ setup.py
@@ -8,4 +8,16 @@ setup(
     ext_modules=[Extension("ciso8601", ["module.c"])],
     test_suite='tests',
     tests_require=['pytz'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
