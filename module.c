de131c347b32caaccbc5d26a27247ad3f06db153
Michael Overmeyer <m.overmeyer@yahoo.ca>
2018-06-04 07:42:04 -0400

    Fixed compiler warnings
    - Wrapped Python version specific variables in `ifdef`'s
    - Changed type in PyErr_Format to please Python 2.7
    - Added Python requested metadata to setup.py

--- module.c
+++ module.c
@@ -1,15 +1,18 @@
 #include <Python.h>
 #include <datetime.h>
 
-static PyObject *fixed_offset;
-static PyObject *utc;
-
 #define PY_VERSION_AT_LEAST_32 \
     ((PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION >= 2) || PY_MAJOR_VERSION > 3)
 #define PY_VERSION_AT_LEAST_36 \
     ((PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION >= 6) || PY_MAJOR_VERSION > 3)
 #define PY_VERSION_AT_LEAST_37 \
     ((PY_MAJOR_VERSION == 3 && PY_MINOR_VERSION >= 7) || PY_MAJOR_VERSION > 3)
+
+#if !PY_VERSION_AT_LEAST_37
+static PyObject *fixed_offset;
+#endif
+
+static PyObject *utc;
 
 #define PARSE_INTEGER(field, length, field_name)                       \
     for (i = 0; i < length; i++) {                                     \
@@ -43,15 +46,15 @@ static PyObject *utc;
     /* If we break early, fully expand the usecond */          \
     while (i++ < 6) usecond *= 10;
 
-#define PARSE_SEPARATOR(separator, field_name)                               \
-    if (separator) {                                                         \
-        c++;                                                                 \
-    }                                                                        \
-    else {                                                                   \
-        PyErr_Format(PyExc_ValueError,                                       \
-                     "Invalid character while parsing %s ('%c', Index: %d)", \
-                     field_name, *c, (c - str) / sizeof(char));              \
-        return NULL;                                                         \
+#define PARSE_SEPARATOR(separator, field_name)                                \
+    if (separator) {                                                          \
+        c++;                                                                  \
+    }                                                                         \
+    else {                                                                    \
+        PyErr_Format(PyExc_ValueError,                                        \
+                     "Invalid character while parsing %s ('%c', Index: %lu)", \
+                     field_name, *c, (c - str) / sizeof(char));               \
+        return NULL;                                                          \
     }
 
 static void *
@@ -411,8 +414,12 @@ PyInit_ciso8601(void)
 initciso8601(void)
 #endif
 {
+#if !PY_VERSION_AT_LEAST_32
     PyObject *pytz;
+#elif !PY_VERSION_AT_LEAST_37
     PyObject *datetime;
+#endif
+
 #if PY_MAJOR_VERSION >= 3
     PyObject *module = PyModule_Create(&moduledef);
 #else
