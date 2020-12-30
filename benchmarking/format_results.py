6cd68f6e9c4b1f3a013a8d5085b81e268b701f2a
Michael Overmeyer <m.overmeyer@yahoo.ca>
2020-12-30 10:53:10 -0500

    Refactor format_results to be cleaner

--- benchmarking/format_results.py
+++ benchmarking/format_results.py
@@ -4,15 +4,33 @@ import os
 import platform
 import pytablewriter
 import re
-import sys
 
-from collections import defaultdict, namedtuple, UserDict
+from collections import defaultdict, UserDict
 
-Result = namedtuple('Result', ['timing', 'parsed_value', 'exception', 'matched_expected'])
+
+class Result:
+    def __init__(self, timing, parsed_value, exception, matched_expected):
+        self.timing = timing
+        self.parsed_value = parsed_value
+        self.exception = exception
+        self.matched_expected = matched_expected
+
+    def formatted_timing(self):
+        return format_duration(self.timing) if self.timing is not None else ""
+
+    def __str__(self):
+        if self.exception:
+            return f"Raised  ``{self.exception}`` Exception"
+        elif not self.matched_expected:
+            return f"**Incorrect Result** (``{self.parsed_value}``)"
+        else:
+            return self.formatted_timing()
+
 
 class ModuleResults(UserDict):
     def most_modern_result(self):
-        return sorted(self.data.items(), key=lambda kvp: kvp[0], reverse=True)[0]
+        non_exception_results = [(_python_version, result) for _python_version, result in self.data.items() if result.exception is None]
+        return sorted(non_exception_results, key=lambda kvp: kvp[0], reverse=True)[0][1]
 
 FILENAME_REGEX_RAW = r"benchmark_timings_python(\d)(\d).csv"
 FILENAME_REGEX = re.compile(FILENAME_REGEX_RAW)
@@ -24,6 +42,7 @@ UNITS = {"nsec": 1e-9, "usec": 1e-6, "msec": 1e-3, "sec": 1.0}
 SCALES = sorted([(scale, unit) for unit, scale in UNITS.items()], reverse=True)
 
 NOT_APPLICABLE = 'N/A'
+
 
 def format_duration(duration):
     # Based on cPython's `timeit` CLI formatting
@@ -39,21 +58,6 @@ def format_relative(d1, d2):
     return "%.*fx" % (precision, d1 / d2)
 
 
-def determine_used_module_versions(results_directory):
-    module_versions_used = defaultdict(dict)
-    for parent, _dirs, files in os.walk(results_directory):
-        files_to_process = [f for f in files if MODULE_VERSION_FILENAME_REGEX.match(f)]
-        for csv_file in files_to_process:
-            with open(os.path.join(parent, csv_file), 'r') as fin:
-                reader = csv.reader(fin, delimiter=",", quotechar='"')
-                major, minor = next(reader)
-                for module, version in reader:
-                    if version not in module_versions_used[module]:
-                        module_versions_used[module][version] = set()
-                    module_versions_used[module][version].add('.'.join((major, minor)))
-    return module_versions_used
-
-
 def format_used_module_versions(module_versions_used):
     results = []
     for module, versions in sorted(module_versions_used.items(), key=lambda x: x[0].lower()):
@@ -64,16 +68,6 @@ def format_used_module_versions(module_versions_used):
     return results
 
 
-def format_result(result):
-    if result == NOT_APPLICABLE:
-        return NOT_APPLICABLE
-    elif result.exception:
-        return f"Raised  ``{result.exception}`` Exception"
-    elif not result.matched_expected:
-        return f"**Incorrect Result** (``{result.parsed_value}``)"
-    else:
-        return format_duration(result.timing)
-
 def relative_slowdown(subject, comparison):
     most_modern_common_version = next(iter(sorted(set(subject.keys()).intersection(set(comparison)), reverse=True)), None)
 
@@ -82,47 +76,57 @@ def relative_slowdown(subject, comparison):
 
     return format_relative(subject[most_modern_common_version].timing, comparison[most_modern_common_version].timing)
 
-def main(results_directory, output_file, compare_to, include_call, module_version_output):
+
+def filepaths(directory, condition):
+    return [os.path.join(parent, f) for parent, _dirs, files in os.walk(directory) for f in files if condition(f)]
+
+def load_benchmarking_results(results_directory):
     calling_code = {}
     timestamps = set()
     python_versions = set()
     results = defaultdict(ModuleResults)
 
-    for parent, _dirs, files in os.walk(results_directory):
-        files_to_process = [f for f in files if FILENAME_REGEX.match(f)]
-        for csv_file in files_to_process:
-            try:
-                with open(os.path.join(parent, csv_file), 'r') as fin:
-                    reader = csv.reader(fin, delimiter=",", quotechar='"')
-                    major, minor, timestamp = next(reader)
-                    timestamps.add(timestamp)
-                    for module, _setup, stmt, parse_result, count, time_taken, matched, exception in reader:
-                        results[module][(major, minor)] = Result(float(time_taken) / int(count),
-                                                                    parse_result,
-                                                                    exception,
-                                                                    True if matched == "True" else False
-                                                                    )
-                        python_versions.add((major, minor))
-                        calling_code[module] = f"``{stmt.format(timestamp=timestamp)}``"
-            except:
-                print(f"Problem while parsing `{os.path.join(parent, csv_file)}`")
-                raise
-
+    files_to_process = filepaths(results_directory, FILENAME_REGEX.match)
+    for csv_file in files_to_process:
+        try:
+            with open(csv_file, "r") as fin:
+                reader = csv.reader(fin, delimiter=",", quotechar='"')
+                major, minor, timestamp = next(reader)
+                timestamps.add(timestamp)
+                for module, _setup, stmt, parse_result, count, time_taken, matched, exception in reader:
+                    timing = float(time_taken) / int(count) if exception == "" else None
+                    exception = exception if exception != "" else None
+                    results[module][(major, minor)] = Result(
+                        timing,
+                        parse_result,
+                        exception,
+                        matched == "True"
+                    )
+                    python_versions.add((major, minor))
+                    calling_code[module] = f"``{stmt.format(timestamp=timestamp)}``"
+        except Exception:
+            print(f"Problem while parsing `{csv_file}`")
+            raise
 
     if len(timestamps) > 1:
         raise NotImplementedError(f"Found a mix of files in the results directory. Found files that represent the parsing of {timestamps}. Support for handling multiple timestamps is not implemented.")
 
     python_versions_by_modernity = sorted(python_versions, reverse=True)
-    modules_by_modern_speed = [module for module, results in sorted([*results.items()], key=lambda kvp: kvp[1].most_modern_result()[1].timing)]
+    return results, python_versions_by_modernity, calling_code
+
+
+def write_benchmarking_results(results_directory, output_file, baseline_module, include_call):
+    results, python_versions_by_modernity, calling_code = load_benchmarking_results(results_directory)
+    modules_by_modern_speed = [module for module, results in sorted([*results.items()], key=lambda kvp: kvp[1].most_modern_result().timing)]
 
     writer = pytablewriter.RstGridTableWriter()
     formatted_python_versions = ["Python {}".format(".".join(key)) for key in python_versions_by_modernity]
-    writer.header_list = ["Module"] + (["Call"] if include_call else []) + formatted_python_versions + [f"Relative Slowdown (versus {compare_to}, latest Python)"]
+    writer.header_list = ["Module"] + (["Call"] if include_call else []) + formatted_python_versions + [f"Relative Slowdown (versus {baseline_module}, latest Python)"]
     writer.type_hint_list = [pytablewriter.String] * len(writer.header_list)
 
     calling_codes = [calling_code[module] for module in modules_by_modern_speed]
-    performance_results = [[format_result(results[module].get(python_version, NOT_APPLICABLE)) for python_version in python_versions_by_modernity] for module in modules_by_modern_speed]
-    relative_slowdowns = [relative_slowdown(results[module], results[compare_to]) if module != compare_to else NOT_APPLICABLE for module in modules_by_modern_speed]
+    performance_results = [[results[module].get(python_version, NOT_APPLICABLE) for python_version in python_versions_by_modernity] for module in modules_by_modern_speed]
+    relative_slowdowns = [relative_slowdown(results[module], results[baseline_module]) if module != baseline_module else NOT_APPLICABLE for module in modules_by_modern_speed]
 
     writer.value_matrix = [
         [module] + ([calling_code[module]] if include_call else []) + performance_by_version + [relative_slowdown] for module, calling_code, performance_by_version, relative_slowdown in zip(modules_by_modern_speed, calling_codes, performance_results, relative_slowdowns)
@@ -133,26 +137,51 @@ def main(results_directory, output_file, compare_to, include_call, module_versio
         writer.write_table()
         fout.write('\n')
 
-        if modules_by_modern_speed[0] == compare_to:
-            fout.write(f"{compare_to} takes {format_duration(results[compare_to].most_modern_result()[1].timing)}, which is **{relative_slowdown(results[modules_by_modern_speed[1]], results[compare_to])} faster than {modules_by_modern_speed[1]}**, the next fastest ISO 8601 parser in this comparison.\n")
-        else:
-            fout.write(f"{compare_to} takes {format_duration(results[compare_to].most_modern_result()[1].timing)}, which is **{relative_slowdown(results[compare_to], results[modules_by_modern_speed[0]])} slower than {modules_by_modern_speed[0]}**, the fastest ISO 8601 parser in this comparison.\n")
+        if len(modules_by_modern_speed) > 1:
+            baseline_module_timing = results[baseline_module].most_modern_result().formatted_timing()
 
-    with open(os.path.join(os.path.dirname(output_file), module_version_output), 'w') as fout:
+            fastest_module, next_fastest_module = modules_by_modern_speed[0:2]
+            if fastest_module == baseline_module:
+                fout.write(f"{baseline_module} takes {baseline_module_timing}, which is **{relative_slowdown(results[next_fastest_module], results[baseline_module])} faster than {next_fastest_module}**, the next fastest ISO 8601 parser in this comparison.\n")
+            else:
+                fout.write(f"{baseline_module} takes {baseline_module_timing}, which is **{relative_slowdown(results[baseline_module], results[fastest_module])} slower than {fastest_module}**, the fastest ISO 8601 parser in this comparison.\n")
+
+
+def load_module_version_info(results_directory):
+    module_versions_used = defaultdict(dict)
+    files_to_process = filepaths(results_directory, MODULE_VERSION_FILENAME_REGEX.match)
+    for csv_file in files_to_process:
+        with open(csv_file, "r") as fin:
+            reader = csv.reader(fin, delimiter=",", quotechar='"')
+            major, minor = next(reader)
+            for module, version in reader:
+                if version not in module_versions_used[module]:
+                    module_versions_used[module][version] = set()
+                module_versions_used[module][version].add(".".join((major, minor)))
+    return module_versions_used
+
+
+def write_module_version_info(results_directory, output_file):
+    with open(output_file, 'w') as fout:
         fout.write(f"Tested on {platform.system()} {platform.release()} using the following modules:\n")
         fout.write('\n')
         fout.write(".. code:: python\n")
         fout.write('\n')
-        for module_version_line in format_used_module_versions(determine_used_module_versions(results_directory)):
+        for module_version_line in format_used_module_versions(load_module_version_info(results_directory)):
             fout.write(f"  {module_version_line}\n")
+
+
+def main(results_directory, output_file, baseline_module, include_call, module_version_output):
+    write_benchmarking_results(results_directory, output_file, baseline_module, include_call)
+    write_module_version_info(results_directory, os.path.join(os.path.dirname(output_file), module_version_output))
 
 
 if __name__ == '__main__':
     OUTPUT_FILE_HELP = "The filepath to use when outputting the reStructuredText results."
     RESULTS_DIR_HELP = f"Which directory the script should look in to find benchmarking results. Will process any file that match the regexes '{FILENAME_REGEX_RAW}' and '{MODULE_VERSION_FILENAME_REGEX_RAW}'."
 
-    BASE_LIBRARY_DEFAULT = "ciso8601"
-    BASE_LIBRARY_HELP = f"The module to make all relative calculations relative to (default: \"{BASE_LIBRARY_DEFAULT}\")."
+    BASELINE_LIBRARY_DEFAULT = "ciso8601"
+    BASELINE_LIBRARY_HELP = f"The module to make all relative calculations relative to (default: \"{BASELINE_LIBRARY_DEFAULT}\")."
 
     INCLUDE_CALL_DEFAULT = False
     INCLUDE_CALL_HELP = f"Whether or not to include a column showing the actual code call (default: {INCLUDE_CALL_DEFAULT})."
@@ -163,7 +192,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser("Formats the benchmarking results into a nicely formatted block of reStructuredText for use in the README.")
     parser.add_argument("RESULTS", help=RESULTS_DIR_HELP)
     parser.add_argument("OUTPUT", help=OUTPUT_FILE_HELP)
-    parser.add_argument("--base-module", required=False, default=BASE_LIBRARY_DEFAULT, help=BASE_LIBRARY_HELP)
+    parser.add_argument("--baseline-module", required=False, default=BASELINE_LIBRARY_DEFAULT, help=BASELINE_LIBRARY_HELP)
     parser.add_argument("--include-call", required=False, type=bool, default=INCLUDE_CALL_DEFAULT, help=INCLUDE_CALL_HELP)
     parser.add_argument("--module-version-output", required=False, default=MODULE_VERSION_OUTPUT_FILE_DEFAULT, help=MODULE_VERSION_OUTPUT_FILE_HELP)
 
@@ -172,4 +201,4 @@ if __name__ == '__main__':
     if not os.path.exists(args.RESULTS):
         raise ValueError(f'Results directory "{args.RESULTS}" does not exist.')
 
-    main(args.RESULTS, args.OUTPUT, args.base_module, args.include_call, args.module_version_output)
+    main(args.RESULTS, args.OUTPUT, args.baseline_module, args.include_call, args.module_version_output)
