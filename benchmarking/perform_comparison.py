eb22bf490a9c4b213bfea9f0030005fd6ac73cd3
Michael Overmeyer <michael.overmeyer@shopify.com>
2021-06-26 10:42:07 -0400

    Autoformat code
    Taking the bits I like from black, then using autopep8

--- benchmarking/perform_comparison.py
+++ benchmarking/perform_comparison.py
@@ -1,11 +1,13 @@
 import argparse
 import csv
 import os
-import pytz
 import sys
 import timeit
 
 from datetime import datetime
+
+import pytz
+
 
 try:
     from importlib.metadata import version as get_module_version
@@ -13,31 +15,32 @@ except ImportError:
     from importlib_metadata import version as get_module_version
 
 ISO_8601_MODULES = {
-    "aniso8601": ('import aniso8601', "aniso8601.parse_datetime('{timestamp}')"),
-    "ciso8601": ('import ciso8601', "ciso8601.parse_datetime('{timestamp}')"),
-    "python-dateutil": ('import dateutil.parser', "dateutil.parser.parse('{timestamp}')"),
-    "iso8601": ('import iso8601', "iso8601.parse_date('{timestamp}')"),
-    "iso8601utils": ('from iso8601utils import parsers', "parsers.datetime('{timestamp}')"),
-    "isodate": ('import isodate', "isodate.parse_datetime('{timestamp}')"),
-    "maya": ('import maya', "maya.parse('{timestamp}').datetime()"),
-    "pendulum": ('from pendulum.parsing import parse_iso8601', "parse_iso8601('{timestamp}')"),
-    "PySO8601": ('import PySO8601', "PySO8601.parse('{timestamp}')"),
-    "str2date": ('from str2date import str2date', "str2date('{timestamp}')"),
+    "aniso8601": ("import aniso8601", "aniso8601.parse_datetime('{timestamp}')"),
+    "ciso8601": ("import ciso8601", "ciso8601.parse_datetime('{timestamp}')"),
+    "python-dateutil": ("import dateutil.parser", "dateutil.parser.parse('{timestamp}')"),
+    "iso8601": ("import iso8601", "iso8601.parse_date('{timestamp}')"),
+    "iso8601utils": ("from iso8601utils import parsers", "parsers.datetime('{timestamp}')"),
+    "isodate": ("import isodate", "isodate.parse_datetime('{timestamp}')"),
+    "maya": ("import maya", "maya.parse('{timestamp}').datetime()"),
+    "pendulum": ("from pendulum.parsing import parse_iso8601", "parse_iso8601('{timestamp}')"),
+    "PySO8601": ("import PySO8601", "PySO8601.parse('{timestamp}')"),
+    "str2date": ("from str2date import str2date", "str2date('{timestamp}')"),
 }
 
-if os.name != 'nt' and (sys.version_info.major, sys.version_info.minor) < (3, 9):
+if os.name != "nt" and (sys.version_info.major, sys.version_info.minor) < (3, 9):
     # udatetime doesn't support Windows.
-    ISO_8601_MODULES["udatetime"] = ('import udatetime', "udatetime.from_string('{timestamp}')")
+    ISO_8601_MODULES["udatetime"] = ("import udatetime", "udatetime.from_string('{timestamp}')")
 
 if sys.version_info.major > 2:
     # zulu no longer supports Python 2.7
-    ISO_8601_MODULES["zulu"] = ('import zulu', "zulu.parse('{timestamp}')")
+    ISO_8601_MODULES["zulu"] = ("import zulu", "zulu.parse('{timestamp}')")
 
 if (sys.version_info.major, sys.version_info.minor) != (3, 4):
     # arrow no longer supports Python 3.4
-    ISO_8601_MODULES["arrow"] = ('import arrow', "arrow.get('{timestamp}').datetime")
+    ISO_8601_MODULES["arrow"] = ("import arrow", "arrow.get('{timestamp}').datetime")
     # moment is built on `times`, which is built on `arrow`, which no longer supports Python 3.4
-    ISO_8601_MODULES["moment"] = ('import moment', "moment.date('{timestamp}').date")
+    ISO_8601_MODULES["moment"] = ("import moment", "moment.date('{timestamp}').date")
+
 
 def check_roughly_equivalent(dt1, dt2):
     # For the purposes of our benchmarking, we don't care if the datetime
@@ -56,19 +59,19 @@ def run_tests(timestamp, results_directory, compare_to):
     auto_range_file_writer = None
     try:
         if (sys.version_info.major == 3 and sys.version_info.minor >= 6) or sys.version_info.major > 3:
-            auto_range_file_obj = open(os.path.join(results_directory, "auto_range_counts.csv"), 'w')
-            auto_range_file_writer = csv.writer(auto_range_file_obj, delimiter=',', quotechar='"', lineterminator='\n')
+            auto_range_file_obj = open(os.path.join(results_directory, "auto_range_counts.csv"), "w")
+            auto_range_file_writer = csv.writer(auto_range_file_obj, delimiter=",", quotechar='"', lineterminator="\n")
         else:
             with open(os.path.join(results_directory, "auto_range_counts.csv"), "r") as fin:
-                reader = csv.reader(fin, delimiter=',', quotechar='"')
+                reader = csv.reader(fin, delimiter=",", quotechar='"')
                 for module, count in reader:
                     test_interation_counts[module] = int(count)
 
         exec(ISO_8601_MODULES[compare_to][0])
         expected_parse_result = eval(ISO_8601_MODULES[compare_to][1].format(timestamp=timestamp))
 
-        with open(os.path.join(results_directory, "benchmark_timings_python{major}{minor}.csv".format(major=sys.version_info.major, minor=sys.version_info.minor)), 'w') as fout:
-            writer = csv.writer(fout, delimiter=',', quotechar='"', lineterminator='\n')
+        with open(os.path.join(results_directory, "benchmark_timings_python{major}{minor}.csv".format(major=sys.version_info.major, minor=sys.version_info.minor)), "w") as fout:
+            writer = csv.writer(fout, delimiter=",", quotechar='"', lineterminator="\n")
             writer.writerow([sys.version_info.major, sys.version_info.minor, timestamp])
             for module, (setup, stmt) in ISO_8601_MODULES.items():
                 count = None
@@ -90,7 +93,18 @@ def run_tests(timestamp, results_directory, compare_to):
                     parse_result = None
                     exception = type(exc)
 
-                writer.writerow([module, setup, stmt.format(timestamp=timestamp), parse_result if parse_result is not None else "None", count, time_taken, check_roughly_equivalent(parse_result, expected_parse_result), exception])
+                writer.writerow(
+                    [
+                        module,
+                        setup,
+                        stmt.format(timestamp=timestamp),
+                        parse_result if parse_result is not None else "None",
+                        count,
+                        time_taken,
+                        check_roughly_equivalent(parse_result, expected_parse_result),
+                        exception,
+                    ]
+                )
 
                 if auto_range_file_writer is not None:
                     auto_range_file_writer.writerow([module, count])
@@ -98,21 +112,21 @@ def run_tests(timestamp, results_directory, compare_to):
         if auto_range_file_obj is not None:
             auto_range_file_obj.close()
 
-    with open(os.path.join(results_directory, "module_versions_python{major}{minor}.csv".format(major=sys.version_info.major, minor=sys.version_info.minor)), 'w') as fout:
-        module_version_writer = csv.writer(fout, delimiter=',', quotechar='"', lineterminator='\n')
+    with open(os.path.join(results_directory, "module_versions_python{major}{minor}.csv".format(major=sys.version_info.major, minor=sys.version_info.minor)), "w") as fout:
+        module_version_writer = csv.writer(fout, delimiter=",", quotechar='"', lineterminator="\n")
         module_version_writer.writerow([sys.version_info.major, sys.version_info.minor])
         for module, (setup, stmt) in sorted(ISO_8601_MODULES.items(), key=lambda x: x[0].lower()):
             module_version_writer.writerow([module, get_module_version(module)])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     TIMESTAMP_HELP = "Which ISO 8601 timestamp to parse"
 
     BASE_LIBRARY_DEFAULT = "ciso8601"
-    BASE_LIBRARY_HELP = "The module to make correctness decisions relative to (default: \"{default}\").".format(default=BASE_LIBRARY_DEFAULT)
+    BASE_LIBRARY_HELP = 'The module to make correctness decisions relative to (default: "{default}").'.format(default=BASE_LIBRARY_DEFAULT)
 
     RESULTS_DIR_DEFAULT = "benchmark_results"
-    RESULTS_DIR_HELP = "Which directory the script should output benchmarking results. (default: \"{0}\")".format(RESULTS_DIR_DEFAULT)
+    RESULTS_DIR_HELP = 'Which directory the script should output benchmarking results. (default: "{0}")'.format(RESULTS_DIR_DEFAULT)
 
     parser = argparse.ArgumentParser("Runs `timeit` to benchmark a variety of ISO 8601 parsers.")
     parser.add_argument("TIMESTAMP", help=TIMESTAMP_HELP)
--- setup.py
+++ setup.py
@@ -1,69 +1,73 @@
 import os
 
 from setuptools import setup, Extension
+
 # workaround for open() with encoding='' python2/3 compatibility
 from io import open
 
-with open('README.rst', encoding='utf-8') as file:
+with open("README.rst", encoding="utf-8") as file:
     long_description = file.read()
 
 # We want to force all warnings to be considered errors. That way we get to catch potential issues during
 # development and at PR review time.
 # But since ciso8601 is a source distribution, exotic compiler configurations can cause spurious warnings that
 # would fail the installation. So we only want to treat warnings as errors during development.
-if os.environ.get("STRICT_WARNINGS", '0') == '1':
+if os.environ.get("STRICT_WARNINGS", "0") == "1":
     # We can't use `extra_compile_args`, since the cl.exe (Windows) and gcc compilers don't use the same flags.
     # Further, there is not an easy way to tell which compiler is being used.
     # Instead we rely on each compiler looking at their appropriate environment variable.
 
     # GCC/Clang
     try:
-        _ = os.environ['CFLAGS']
+        _ = os.environ["CFLAGS"]
     except KeyError:
-        os.environ['CFLAGS'] = ""
-    os.environ['CFLAGS'] += " -Werror"
+        os.environ["CFLAGS"] = ""
+    os.environ["CFLAGS"] += " -Werror"
 
     # cl.exe
     try:
-        _ = os.environ['_CL_']
+        _ = os.environ["_CL_"]
     except KeyError:
-        os.environ['_CL_'] = ""
-    os.environ['_CL_'] += " /WX"
+        os.environ["_CL_"] = ""
+    os.environ["_CL_"] += " /WX"
 
 VERSION = "2.1.3"
 
 setup(
     name="ciso8601",
     version=VERSION,
-    description='Fast ISO8601 date time parser for Python written in C',
+    description="Fast ISO8601 date time parser for Python written in C",
     long_description=long_description,
     url="https://github.com/closeio/ciso8601",
     license="MIT",
-    ext_modules=[Extension("ciso8601",
-                           sources=["module.c"],
-                           define_macros=[("CISO8601_VERSION", VERSION)]
-                           )],
+    ext_modules=[
+        Extension(
+            "ciso8601",
+            sources=["module.c"],
+            define_macros=[("CISO8601_VERSION", VERSION)],
+        )
+    ],
     packages=["ciso8601"],
     package_data={"ciso8601": ["__init__.pyi", "py.typed"]},
-    test_suite='tests',
+    test_suite="tests",
     tests_require=[
-        'pytz',
-        "unittest2 ; python_version < '3'"
+        "pytz",
+        "unittest2 ; python_version < '3'",
     ],
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
