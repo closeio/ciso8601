fdc71c4288a575dd476c8308ce3b17a716165435
Michael Overmeyer <michael@overmeyer.io>
2023-10-04 09:30:27 -0400

    Update to tox v4

--- benchmarking/run_benchmarks.sh
+++ benchmarking/run_benchmarks.sh
@@ -1,5 +1,5 @@
-tox '2014-01-09T21:48:00'
-tox '2014-01-09T21:48:00-05:30'
+tox -- '2014-01-09T21:48:00'
+tox -- '2014-01-09T21:48:00-05:30'
 python format_results.py benchmark_results/2014-01-09T214800 benchmark_results/benchmark_with_no_time_zone.rst
 python format_results.py benchmark_results/2014-01-09T214800-0530 benchmark_results/benchmark_with_time_zone.rst
 python rst_include_replace.py ../README.rst 'benchmark_with_no_time_zone.rst' benchmark_results/benchmark_with_no_time_zone.rst
